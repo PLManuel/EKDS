ed0a98c67a5eeae3319311fbf6e3c3c2d7111d81
PLManuel <manuel715pl@gmail.com>
2025-06-29 04:39:24 -0500

    elimina archivos sin uso

--- public/js/scriptFlexbox.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const libreriaMenu = document.querySelector('.tienda-menu');   
-    const navLinks = document.querySelector('.button-container');
-
-    libreriaMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-});
